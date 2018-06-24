7054e71893192dae96b9daaaa942363fa78b6b18
enterpriseinternproject <40547714+enterpriseinternproject@users.noreply.github.com>
2018-06-24 16:02:57 -0500

    no clue if this JS works

--- js/main.js
+++ js/main.js
@@ -61,6 +61,15 @@
 
         return check;
     });
+    
+    var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+    output.innerHTML = this.value;
+}
 
 
     $('.validate-form .input3').each(function(){
